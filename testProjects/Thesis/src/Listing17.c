0741322d78c047c3bf01bbecd47f55dda57315af
LPhD <gerling@sse.uni-hildesheim.de>
2023-05-09 16:54:04 +0200

    Added smaller example for pre analysis

---
+++ testProjects/Thesis/src/Listing17.c
@@ -0,0 +1,9 @@
+#define dprint(x) printf("\n%s at %s:%i is %i",#x,__FILE__,__LINE__,x);
+
+int compute(int value){
+    value = value * value;
+#ifdef DEBUG
+    dprint(value)
+#endif
+    return value;
+}
